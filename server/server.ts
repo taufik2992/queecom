f4f965af96e0d8b46e8fb422527cc9891bab68a2
unknown <taufikhidayat0032@gmail.com>
2026-04-15 01:48:10 +0700

    clekwebhook

--- server/server.ts
+++ server/server.ts
@@ -3,20 +3,14 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
+import { clerkWebhook } from "./controllers/webhooks.js";
 
 const app = express();
 
 //connect to MongoDB
 await connectDB();
 
-app.post(
-  "/api/clerk",
-  express.raw({ type: "application/json" }),
-  (req: Request, res: Response) => {
-    console.log("Received request body:", req.body);
-    res.status(200).json({ message: "Clerk webhook received" });
-  },
-);
+app.post("/api/clerk", express.raw({ type: "application/json" }), clerkWebhook);
 
 //middleware
 app.use(cors());
